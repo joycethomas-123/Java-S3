8f77b50699613baace016b7e330aea89fe48c95b
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-01-23 22:19:47 +0530

    Create Mularrays.java

---
+++ Cycle 2/Mularrays.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Mularrays
+{
+  public static void main(String args[])
+  {
+    int row,col,i,j;
+    System.out.println("Enter number of rows");
+    Scanner in=new Scanner(System.in);
+    row=in.nextInt();
+    System.out.println("Enter number of columns");
+    col=in.nextInt();
+    int mat1[][]=new int[row][col];
+    int mat2[][]=new int[row][col];
+    int res[][]=new int[row][col];
+    System.out.println("Enter elements in matrix 1");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            mat1[i][j]=in.nextInt();
+            System.out.println();
+        }
+    }
+    System.out.println("Enter elements in matrix 2");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            mat2[i][j]=in.nextInt();
+            System.out.println();
+        }
+    }
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            res[i][j]=mat1[i][j]*mat2[i][j];
+        }
+    }
+    System.out.println("Result= \n");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            System.out.println(+res[i][j]);
+        }
+    }
+    
+   }
+  }
