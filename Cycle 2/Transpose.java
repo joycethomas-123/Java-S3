17299018d485a5b3a0cbd656d7b72251fdc73909
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-01-24 14:15:43 +0530

    Create Transpose.java

---
+++ Cycle 2/Transpose.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Main
+{
+  public static void main(String args[])
+  {
+    int row,col,i,j;
+    System.out.println("Enter number of rows");
+    Scanner in=new Scanner(System.in);
+    row=in.nextInt();
+    System.out.println("Enter number of columns");
+    col=in.nextInt();
+    int a[][]=new int[row][col];
+    System.out.println("Enter elements in matrix ");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            a[i][j]=in.nextInt();
+            System.out.println();
+        }
+    }
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            if(i==j)
+            {
+                a[j][i]=a[i][j];
+            }
+        }
+    }
+    System.out.println("Transpose of matrix is:\n");
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            System.out.println(+a[j][i]);
+        }
+    }
+    
+   }
+  }
+  
