6207bc1fd61c0366cb8f742ee98c649b2bc1c1a9
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-01-25 20:16:34 +0530

    Create Perimeter.java

---
+++ Cycle 3/Perimeter.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter radius of circle: ");
+        int a = obj.nextInt();
+        System.out.print("Enter length of rectangle: ");
+        int b = obj.nextInt();
+        System.out.print("Enter breadth of rectangle: ");
+        int c = obj.nextInt();
+        Perimeter obj1 = new Perimeter();
+        obj1.Perimeter(a);
+        obj1.Perimeter(b,c);
+    }
+    
+}
+
+class Perimeter
+{
+    void Perimeter(int r)
+    { 
+        double perimeter;
+        perimeter=2*3.14*r;
+        System.out.println("Perimeter of circle is: "+perimeter);
+    }
+    void Perimeter(int l,int b)
+    {
+        int perimeter;
+        perimeter=2*(l+b);
+        System.out.println("Perimeter of rectangle is: "+perimeter);
+    }
+    
+}
