1b015e7acab01782baa62d3c74cf75672fb6e80d
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-01-26 08:55:46 +0530

    Create Numstring.java

---
+++ Cycle 3/Numstring.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        String word1,word2;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter first integer:");
+        a = obj.nextInt();
+         System.out.print("Enter second integer:");
+        b = obj.nextInt();
+        System.out.print("Enter first  word: ");
+        word1 = obj.next();
+        System.out.print("Enter second word: ");
+        word2 = obj.next();
+        
+        
+       
+        Numstring obj1 = new Numstring();
+        obj1.Concatenate(a,b);
+        obj1.Concatenate(word1,word2);
+    }
+}
+
+class Numstring 
+{
+    void Concatenate(int a,int b)
+       { 
+            System.out.println("Concatenated numbers are:" +a+b);
+       }
+    
+    void Concatenate(String word1,String word2)  
+       {
+            System.out.println("Concatenated words are:" +word1+word2);
+       }
+}
